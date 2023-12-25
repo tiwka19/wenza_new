d77af2a4e974468b50e8d677bce9907a39a410a4
tiwwwka <61656257+tiwka19@users.noreply.github.com>
2023-12-25 09:20:01 +0300

    test

---
+++ src/js/files/forms.js
@@ -0,0 +1,206 @@
+import JustValidate from 'just-validate';
+
+const phoneInput = document.querySelector('#phone');
+if (phoneInput) {
+  phoneInput.addEventListener('input', function (event) {
+    const inputValue = this.value;
+    const allowedRegex = /^[0-9\+]*$/;
+    if (!allowedRegex.test(inputValue)) {
+      this.value = inputValue.slice(0, -1);
+    }
+  });
+}
+
+const formValidations = () => {
+  if (document.querySelector('#form')) {
+    let validation = new JustValidate('#form');
+
+    if (document.querySelector('#companyName')) {
+      validation
+        .addField(
+          '#companyName',
+          [
+            {
+              rule: 'required',
+            },
+          ],
+          {
+            errorsContainer: document.querySelector('error-name-container'),
+          },
+        )
+        .addField(
+          '#name',
+          [
+            {
+              rule: 'required',
+            },
+            {
+              rule: 'maxLength',
+              value: 30,
+            },
+          ],
+          {
+            errorsContainer: document.querySelector('error-name-container'),
+          },
+        )
+        .addField(
+          '#email',
+          [
+            {
+              rule: 'required',
+              errorMessage: 'Email is required',
+            },
+            {
+              rule: 'email',
+              errorMessage: 'Email is invalid!',
+            },
+          ],
+          {
+            errorsContainer: document.querySelector('error-email-container'),
+          },
+        )
+        .addField(
+          '#phone',
+          [
+            {
+              rule: 'required',
+              errorMessage: 'Phone is required',
+            },
+            {
+              rule: 'number',
+              errorMessage: 'Phone is invalid!',
+            },
+          ],
+          {
+            errorsContainer: document.querySelector('error-email-container'),
+          },
+        );
+    } else {
+      validation
+        .addField(
+          '#name',
+          [
+            {
+              rule: 'required',
+            },
+            {
+              rule: 'maxLength',
+              value: 30,
+            },
+          ],
+          {
+            errorsContainer: document.querySelector('error-name-container'),
+          },
+        )
+        .addField(
+          '#email',
+          [
+            {
+              rule: 'required',
+              errorMessage: 'Email is required',
+            },
+            {
+              rule: 'email',
+              errorMessage: 'Email is invalid!',
+            },
+          ],
+          {
+            errorsContainer: document.querySelector('error-email-container'),
+          },
+        )
+        .addField(
+          '#phone',
+          [
+            {
+              rule: 'required',
+              errorMessage: 'Phone is required',
+            },
+            {
+              rule: 'number',
+              errorMessage: 'Phone is invalid!',
+            },
+          ],
+          {
+            errorsContainer: document.querySelector('error-email-container'),
+          },
+        );
+    }
+  }
+};
+
+formValidations();
+
+const requestForm = () => {
+  const fileInputs = document.querySelectorAll('input[type="file"]');
+  const form = document.querySelector('.request .wpcf7-form');
+  if (!form) return;
+  fileInputs.forEach((fileInput) => {
+    fileInput.addEventListener('change', () => {
+      if (fileInput.files.length > 0) {
+        const fileName = fileInput.files[0].name;
+        const maxLength = 15;
+        const extension = fileName.substring(fileName.lastIndexOf('.'));
+
+        let displayText = fileName;
+        if (fileName.length > maxLength) {
+          const truncatedName = fileName.substring(0, maxLength - 3);
+          displayText = truncatedName + '...' + extension;
+        }
+        fileInput.nextElementSibling.classList.add('_active');
+        fileInput.nextElementSibling.querySelector('span').textContent = displayText;
+      } else {
+        fileInput.nextElementSibling.classList.remove('remove');
+        fileInput.nextElementSibling.textContent = fileInput.files[0].name;
+        fileInput.nextElementSibling.querySelector('span').textContent = 'upload a photo';
+      }
+    });
+    fileInput.nextElementSibling.querySelector('.remove-button').addEventListener('click', () => {
+      fileInput.value = '';
+      fileInput.nextElementSibling.classList.remove('_active');
+      fileInput.nextElementSibling.querySelector('span').textContent = 'upload a photo';
+    });
+  });
+
+  form.addEventListener(
+    'wpcf7mailsent',
+    (e) => {
+      fileInputs.forEach((fileInput) => {
+        fileInput.value = '';
+        fileInput.nextElementSibling.classList.remove('_active');
+        fileInput.nextElementSibling.querySelector('span').textContent = 'upload a photo';
+      });
+    },
+    false,
+  );
+
+  const validator = new JustValidate(form);
+  const inputs = document.querySelectorAll('.request__input');
+  inputs.forEach((input) => {
+    validator.addField(input, [{ rule: 'required' }]);
+  });
+  validator.addField('#email', [{ rule: 'email' }]);
+};
+requestForm();
+
+const formUpload = () => {
+  const actualButton = document.querySelector('#uploadInput');
+  if (!actualButton) return;
+  const uploadButton = document.querySelector('#uploadButton');
+  const fileNameSpan = uploadButton.querySelector('span');
+  const closeIcon = document.querySelector('#closeIcon');
+
+  actualButton.addEventListener('change', function () {
+    const fileName = this.files[0].name;
+    fileNameSpan.textContent = fileName;
+    document.querySelector('.form__callback-upload').classList.add('active');
+    uploadButton.classList.add('active');
+  });
+
+  closeIcon.addEventListener('click', function () {
+    actualButton.value = '';
+    uploadButton.classList.remove('active');
+    fileNameSpan.textContent = 'upload your CV';
+    document.querySelector('.form__callback-upload').classList.remove('active');
+  });
+};
+formUpload();
